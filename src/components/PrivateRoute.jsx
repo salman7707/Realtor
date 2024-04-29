3a21725a91475590669d6a4ea47ba7647fa1137b
Attridevelopers <salmanaliofficial7@gmail.com>
2024-04-29 14:09:21 +0500

    change

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router'
 import { CheckingAuthUser } from '../hooks/CheckingAuthUser'
-import Spinner from './Spinner'
+// import Spinner from './Spinner'
 
 export default function PrivateRoute() {
   const {loggedIn, checkingStatus} = CheckingAuthUser()
   if(checkingStatus){
-     return <Spinner />
+     return <p>Loading...</p>
   }
     return loggedIn ? <Outlet/> : <Navigate to="/SignIn" /> 
 }
